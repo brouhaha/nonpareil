069b76da09d58203f95c1d8275d6c12f3101569e
Eric Smith <spacewar@gmail.com>
1995-03-08 03:15:09 +0000

    added warranty

--- symtab.h
+++ symtab.h
@@ -12,6 +12,15 @@ terms of the GNU General Public License version 2 as published by the Free
 Software Foundation.  Note that I am not granting permission to redistribute
 or modify CASM under the terms of any later version of the General Public
 License.
+
+This program is distributed in the hope that it will be useful (or at least
+amusing), but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program (in the file "COPYING"); if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
 void init_symbol_table (void);
