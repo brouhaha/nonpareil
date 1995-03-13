46c97412632f289cf5a634901825722e1e7a04bd
Eric Smith <spacewar@gmail.com>
1995-03-13 02:01:20 +0000

    added display_enable variable

--- csim.c
+++ csim.c
@@ -1,3 +1,26 @@
+/*
+CSIM is a simulator for the processor used in the HP "Classic" series
+of calculators, which includes the HP-35, HP-45, HP-55, HP-65, HP-70,
+and HP-80.
+
+Copyright 1995 Eric L. Smith
+
+CSIM is free software; you can redistribute it and/or modify it under the
+terms of the GNU General Public License version 2 as published by the Free
+Software Foundation.  Note that I am not granting permission to redistribute
+or modify CSIM under the terms of any later version of the General Public
+License.
+
+This program is distributed in the hope that it will be useful (or at least
+amusing), but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program (in the file "COPYING"); if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 #include <stdarg.h>
 #include <stdio.h>
 
@@ -31,6 +54,8 @@ uchar del_rom;
 uchar del_grp;
 
 uchar ret_pc;
+
+int display_enable;
 
 int prev_pc;  /* used to store complete five-digit octal address of instruction */
 
@@ -508,11 +533,13 @@ void op_rom_addr_to_buf (int opcode)
 
 void op_display_off (int opcode)
 {
+  display_enable = 0;
   printf ("display off\n");
 }
 
 void op_display_toggle (int opcode)
 {
+  display_enable = ! display_enable;
   printf ("display toggle\n");
 }
 
@@ -643,6 +670,7 @@ void debugger (void)
   op_clear_reg (0);
   op_clear_s (0);
   p = 0;
+  display_enable = 0;
 
   for (;;)
     {
