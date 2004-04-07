49f37ec41a885899a25db5ae52a944e53bbee955
Eric Smith <spacewar@gmail.com>
2004-04-07 19:40:42 +0000

    Revise header comment on all sources.  Update copyright year, program name, and FSF address.

--- asm.h
+++ asm.h
@@ -1,26 +1,22 @@
 /*
-casm.h
 $Id$
 Copyright 1995, 2004 Eric L. Smith <eric@brouhaha.com>
 
-CASM is an assembler for the processor used in the HP "Classic" series
-of calculators, which includes the HP-35, HP-45, HP-55, HP-65, HP-70,
-and HP-80.
+Nonpareil is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License version 2 as
+published by the Free Software Foundation.  Note that I am not
+granting permission to redistribute or modify CASM under the terms of
+any later version of the General Public License.
 
-CASM is free software; you can redistribute it and/or modify it under the
-terms of the GNU General Public License version 2 as published by the Free
-Software Foundation.  Note that I am not granting permission to redistribute
-or modify CASM under the terms of any later version of the General Public
-License.
+Nonpareil is distributed in the hope that it will be useful (or at
+least amusing), but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+the GNU General Public License for more details.
 
-This program is distributed in the hope that it will be useful (or at least
-amusing), but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
-Public License for more details.
-
-You should have received a copy of the GNU General Public License along with
-this program (in the file "COPYING"); if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+You should have received a copy of the GNU General Public License
+along with this program (in the file "COPYING"); if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+MA 02111, USA.
 */
 
 
--- proc.c
+++ proc.c
@@ -1,7 +1,6 @@
 /*
-proc.c
 $Id$
-Copyright 1995, 2004 Eric L. Smith
+Copyright 1995, 2004 Eric L. Smith <eric@brouhaha.com>
 
 Nonpareil is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License version 2 as
@@ -9,14 +8,15 @@ published by the Free Software Foundation.  Note that I am not
 granting permission to redistribute or modify CASM under the terms of
 any later version of the General Public License.
 
-This program is distributed in the hope that it will be useful (or at least
-amusing), but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
-Public License for more details.
+Nonpareil is distributed in the hope that it will be useful (or at
+least amusing), but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with
-this program (in the file "COPYING"); if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+You should have received a copy of the GNU General Public License
+along with this program (in the file "COPYING"); if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+MA 02111, USA.
 */
 
 #include <stdbool.h>
