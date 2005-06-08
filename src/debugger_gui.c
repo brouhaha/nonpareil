8fc1e3f8594ff61adb4b0144c63ed2cf7c7088ee
Eric Smith <spacewar@gmail.com>
2005-06-08 06:49:01 +0000

    For indexed registers (e.g., stack, active_bank), use multiple lines in window rather than an hbox.

--- src/debugger_gui.c
+++ src/debugger_gui.c
@@ -44,10 +44,11 @@ static gboolean reg_window_visible;
 
 typedef struct
 {
-  int width;
-  GtkWidget *widget;
+  int reg_num;
   const reg_info_t *info;
   int index;  // for arrays
+  int width;
+  GtkWidget *widget;
 } reg_display_t;
 
 static int max_reg;
@@ -73,34 +74,35 @@ static void binary_to_string (char *buf, int bits, uint64_t val)
 
 static void update_register_window (void)
 {
-  int reg_num;
+  int i;
   char buf [80];
   uint64_t val;
 
-  for (reg_num = 0; reg_num < max_reg; reg_num++)
+  for (i = 0; i < max_reg; i++)
     {
-      const reg_info_t *info = reg_display [reg_num].info;
+      const reg_info_t *info = reg_display [i].info;
+
       if (sim_read_register (dsim,
 			     sim_get_first_chip (dsim),  // $$$ CPU only for now
-			     reg_num,
-			     0,
+			     reg_display [i].reg_num,
+			     reg_display [i].index,
 			     & val))
 	{
 	  if (info->display_radix == 16)
-	    snprintf (buf, sizeof (buf), "%0*" PRIx64, reg_display [reg_num].width, val);
+	    snprintf (buf, sizeof (buf), "%0*" PRIx64, reg_display [i].width, val);
 	  else if (info->display_radix == 10)
-	    snprintf (buf, sizeof (buf), "%0*" PRIu64, reg_display [reg_num].width, val);
+	    snprintf (buf, sizeof (buf), "%0*" PRIu64, reg_display [i].width, val);
 	  else if (info->display_radix == 8)
-	    snprintf (buf, sizeof (buf), "%0*" PRIo64, reg_display [reg_num].width, val);
+	    snprintf (buf, sizeof (buf), "%0*" PRIo64, reg_display [i].width, val);
 	  else // binary
-	    binary_to_string (buf, reg_display [reg_num].info->element_bits, val);
+	    binary_to_string (buf, reg_display [i].info->element_bits, val);
 	}
       else
 	snprintf (buf, sizeof (buf), "err");
       if ((info->display_radix == 2) && (info->element_bits == 1))
-	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (reg_display [reg_num].widget), val);
+	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (reg_display [i].widget), val);
       else
-	gtk_entry_set_text (GTK_ENTRY (reg_display [reg_num].widget), buf);
+	gtk_entry_set_text (GTK_ENTRY (reg_display [i].widget), buf);
     }
 }
 
@@ -111,49 +113,55 @@ static int log2tab [17] =
 
 static bool debug_window_add_register (GtkWidget *table, int reg_num)
 {
-  int l2radix;
-  GtkWidget *hbox;
-  int i;
   const reg_info_t *r;
+  int index;
+  int l2radix, width;
+  char reg_name [80];
 
   r = sim_get_register_info (dsim,
 			     sim_get_first_chip (dsim),  // $$$ CPU only for now
 			     reg_num);
   if (! r)
     return false;
-  reg_display [reg_num].info = r;
-
-  gtk_table_attach_defaults (GTK_TABLE (table),
-			     gtk_label_new (r->name),
-			     0,
-			     1,
-			     reg_num,
-			     reg_num + 1);
 
   l2radix = log2tab [r->display_radix];
+  width = (r->element_bits + l2radix - 1) / l2radix;
 
-  reg_display [reg_num].width = (r->element_bits + l2radix - 1) / l2radix;
-
-  hbox = gtk_hbox_new (FALSE, 1);
-
-  for (i = 0; i < r->array_element_count; i++)
+  for (index = 0; index < r->array_element_count; index++)
     {
-      GtkWidget *w;
-      if ((r->display_radix == 2) && (r->element_bits == 1))
-	w = gtk_check_button_new ();
+      reg_display [max_reg].reg_num = reg_num;
+      reg_display [max_reg].info = r;
+      reg_display [max_reg].index = index;
+      reg_display [max_reg].width = width;
+
+      if (r->array_element_count > 1)
+	sprintf (reg_name, "%s [%d]", r->name, index);
       else
-	w = gtk_entry_new_with_max_length (reg_display [reg_num].width);
-      if (i == 0)
-	reg_display [reg_num].widget = w;
-      gtk_box_pack_start (GTK_BOX (hbox), w, FALSE, TRUE, 0);
+	sprintf (reg_name, "%s", r->name);
+
+      gtk_table_attach_defaults (GTK_TABLE (table),
+				 gtk_label_new (reg_name),
+				 0,
+				 1,
+				 max_reg,
+				 max_reg + 1);
+
+      if ((r->display_radix == 2) && (r->element_bits == 1))
+	reg_display [max_reg].widget = gtk_check_button_new ();
+      else
+	reg_display [max_reg].widget = gtk_entry_new_with_max_length (width);
+
+      gtk_table_attach_defaults (GTK_TABLE (table),
+				 reg_display [max_reg].widget,
+				 1,
+				 2,
+				 max_reg,
+				 max_reg + 1);
+
+      max_reg++;
     }
 
-  gtk_table_attach_defaults (GTK_TABLE (table),
-			     hbox,
-			     1,
-			     2,
-			     reg_num,
-			     reg_num + 1);
+
   return true;
 }
 
@@ -187,11 +195,12 @@ void debug_show_reg  (gpointer callback_data,
 
       table = gtk_table_new (1, 2, FALSE);
       gtk_container_add (GTK_CONTAINER (reg_window), table);
+
+      max_reg = 0;
+
       while (debug_window_add_register (table, reg_num))
 	reg_num++;
     }
-
-  max_reg = reg_num;
 
   reg_window_visible = ! reg_window_visible;
 
