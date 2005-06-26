be647032fe894fb0353b754042e110551d589d06
Eric Smith <spacewar@gmail.com>
2005-06-26 02:02:16 +0000

    Use base SDL audio rather than SDL_mixer.

--- src/sound.c
+++ src/sound.c
@@ -21,29 +21,118 @@ MA 02111, USA.
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include "SDL_mixer.h"
+#include "SDL_audio.h"
 
+#include "util.h"
 #include "sound.h"
+
+
+static bool sound_open = false;
+
+
+#define MAX_SOUNDS 4
+typedef struct
+{
+  uint8_t  *data;
+  uint32_t pos;
+  uint32_t len;
+} sound_info_t;
+
+sound_info_t sounds [MAX_SOUNDS];
+
+SDL_AudioSpec hw_fmt;
+
+
+void sound_callback (void *unused, uint8_t *stream, int len)
+{
+  int i;
+  int count;
+
+  for (i = 0; i < MAX_SOUNDS; i++)
+    {
+      count = sounds [i].len - sounds [i].pos;
+      if (count > len)
+	count = len;
+      SDL_MixAudio (stream,
+		    & sounds [i].data [sounds [i].pos],
+		    count,
+		    SDL_MIX_MAXVOLUME);
+      sounds [i].pos += count;
+    }
+}
 
 
 bool init_sound (void)
 {
-  return Mix_OpenAudio (22050,
-			AUDIO_U8,
-			1,
-			32768) == 0;
+  SDL_AudioSpec req_fmt;
+
+  if (sound_open)
+    return true;
+
+  req_fmt.freq = 22050;
+  req_fmt.format = AUDIO_S16,
+  req_fmt.channels = 1;
+  req_fmt.samples = 8192;
+  req_fmt.callback = sound_callback;
+  req_fmt.userdata = NULL;
+
+  if (SDL_OpenAudio (& req_fmt, & hw_fmt) < 0)
+    return false;
+
+  memset (& sounds, 0, sizeof (sounds));
+
+  SDL_PauseAudio (0);
+
+  return true;
 }
 
 
 bool play_sound (const uint8_t *buf, size_t len)
 {
-  Mix_Chunk *chunk;
+  int index;
+  SDL_AudioCVT cvt;
+  int format;
+  uint8_t channels;
+  int freq;
 
-  chunk = Mix_QuickLoad_WAV (buf);
+  for (index = 0; index < MAX_SOUNDS; index++)
+    if (sounds [index].pos == sounds [index].len)
+      break;
 
-  return Mix_PlayChannel (-1, // any channel,
-			  chunk,
-			  0) >= 0; // loops - 0 means play once
+  if (index >= MAX_SOUNDS)
+    return false;
 
+  if (sounds [index].data)
+    free (sounds [index].data);
+
+  buf += 44;  // skip wave, data chunk header
+  len -= 44;
+  // $$$ should parse header
+  format = AUDIO_U8;
+  channels = 1;
+  freq = 22050;
+
+  if (SDL_BuildAudioCVT (& cvt,
+			 format, channels, freq,
+			 hw_fmt.format, hw_fmt.channels, hw_fmt.freq) < 0)
+    return false;
+
+  cvt.buf = alloc (len * cvt.len_mult);
+  cvt.len = len;
+  memcpy (cvt.buf, buf, len);
+
+  SDL_ConvertAudio (& cvt);
+
+  SDL_LockAudio ();
+  sounds [index].data = cvt.buf;
+  sounds [index].len = cvt.len * cvt.len_mult;
+  sounds [index].pos = 0;
+  SDL_UnlockAudio ();
+
+  SDL_PauseAudio (0);
+
+  return true;
 }
