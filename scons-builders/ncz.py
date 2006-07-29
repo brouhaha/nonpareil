bf34528d7f05050329f63669c3b3499b805b31f9
Eric Smith <spacewar@gmail.com>
2006-07-29 02:19:27 +0000

    Added emitter, fixed various bugs.

--- scons-builders/ncz.py
+++ scons-builders/ncz.py
@@ -8,18 +8,32 @@ import os.path
 import re
 import zipfile
 
-image_re = re.compile (r'image\\s+"(\\S+)"', re.M)
-rom_re = re.compile (r'rom\\s+"(\\S+)"', re.M)
+def unique (list):
+    d = {}
+    for i in list:
+        d [i] = 0
+    return d.keys ()
+
+image_re = re.compile (r'image\s+"(\S+)"', re.M)
+rom_re = re.compile (r'rom\s+"(\S+)"', re.M)
 
 def kml_scanner_fn (node, env, path):
+    kpath = os.path.dirname (str (node))
     contents = node.get_contents ()
     images = image_re.findall (contents)
     roms = rom_re.findall (contents)
-    return images + roms
+    files = unique (images + roms)
+    fn = []
+    for f in files:
+        fn.append (env.File (kpath + '/' + f))
+    return fn
 
 kml_scanner = env.Scanner (function = kml_scanner_fn,
                            skeys = ['.kml'])
-env.Append (SCANNERS = kml_scanner)
+
+def ncz_emitter_fn (target, source, env):
+    return (target, source + kml_scanner_fn (source [0], env, None))
+
 
 def ncz_action_fn (target, source, env):
     zf = zipfile.ZipFile (str (target [0]), 'w', zipfile.ZIP_DEFLATED)
@@ -30,7 +44,8 @@ def ncz_action_fn (target, source, env):
 ncz_builder = env.Builder (action = env.Action (ncz_action_fn),
 			   src_suffix = '.kml',
 			   source_scanner = kml_scanner,
-                           suffix = '.ncz')
+                           suffix = '.ncz',
+                           emitter = ncz_emitter_fn)
 
 env.Append (BUILDERS = {'NCZ' : ncz_builder})
 
