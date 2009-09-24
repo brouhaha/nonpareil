382318b2a8f6208b38976460acc3d42efe7a1e3c
Eric Smith <spacewar@gmail.com>
2009-09-24 01:06:54 +0000

    Update copyright notice.

--- src/debugger_gui.c
+++ src/debugger_gui.c
@@ -1,6 +1,6 @@
 /*
 $Id$
-Copyright 1995, 2004, 2005, 2006, 2008 Eric Smith <eric@brouhaha.com>
+Copyright 1995, 2004, 2005, 2006, 2008, 2009 Eric Smith <eric@brouhaha.com>
 
 Nonpareil is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License version 2 as
