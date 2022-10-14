cb1e3950dd7f3a89f383e3ae3b945f803ab65010
Eric Smith <spacewar@gmail.com>
2022-10-14 15:55:24 -0600

    Remove try_to_build().

--- scons/nui.py
+++ scons/nui.py
@@ -40,50 +40,11 @@ kml_scanner = env.Scanner (function = kml_scanner_fn,
                            skeys = ['.kml'])
 
 
-# The following tries to figure out how to build a target file based on
-# its extension and the available builders.
-
-# It attempts to determine the source directory that the build
-# directory builds from.  Ideally we would find that out from the
-# mapping of build_dir to src_dir established by SConscript(), but I
-# haven't yet figured out how to extract that.  For Nonpareil, the
-# build dir for foo/bar is build/foo/bar, so we just remove the first
-# component of the path.
-
-def try_to_build (target_fn, env):
-    target_path_and_base, target_suffix = os.path.splitext (target_fn)
-    target_path, target_base = os.path.split (target_path_and_base)
-    # if the target_fn is in the source tree (not the build tree), we're done
-    if target_path [0:6] != 'build/':
-        return os.path.exists (target_fn);
-    builder_list = env ['BUILDERS']
-    for builder_name in builder_list.keys():
-        builder = builder_list [builder_name]
-        if target_suffix in builder.suffix:
-            for src_suffix in builder.src_suffix:
-                src_path = target_path.split ('/', 1) [1]
-                src_fn = src_path + '/' + target_base + src_suffix
-                if os.path.exists (src_fn):
-                    builder.__call__ (target = target_fn,
-                                      source = src_fn,
-                                      env = env,
-                                      for_signature = False)
-                    return True
-                src_fn = target_path + '/' + target_base + src_suffix
-                if try_to_build (src_fn, env):
-                    builder.__call__ (target = target_fn,
-                                      source = src_fn,
-                                      env = env,
-                                      for_signature = False)
-    return False
-                
-
 def nui_emitter_fn (target, source, env):
     extra_files = kml_scanner_fn (source [0], env, None)
     fnl = []
     for f in extra_files:
         fn = str (f)
-        try_to_build (fn, env)
         fnl.append (fn)
     return (target, source + fnl)
 
