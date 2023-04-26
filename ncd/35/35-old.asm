2f548afd67009df706e5fc143135b77fd3a8850a
Eric Smith <spacewar@gmail.com>
2023-04-25 20:47:20 -0600

    Include 35-old, an older version of the 35 microcode that has the e^x bug.

--- ncd/35/35-old.asm
+++ ncd/35/35-old.asm
@@ -1,8 +1,9 @@
-; 35 ROM "v4" code
-; Copyright 2004, 2005, 2023 Eric Smith <spacewar@gmail.com>
+; 35 ROM "v2" code
+; Copyright 2023 Eric Smith <spacewar@gmail.com>
 ; Based on ROM dump by Peter Monta
-
-; Much of the code is similar to the 45 ROM source code, so labels
+; This ROM version has known bugs in exponential and trigonometric functions.
+;
+; Some of the code is similar to the 45 ROM source code, so labels
 ; have been copied from that.
 ;
 ; Some conditional branch instructions ("if no carry go to") may
@@ -14,29 +15,29 @@
 ; start of ROM @00
 
 key_clr:
-        jsb clr		; JSB used as unconditional goto
+	jsb clr		; JSB used as unconditional goto
 
 l00001:	go to l00277
-	
+
 key_e_to_x:
         0 -> s8
 key_ln:
 	go to l00005
-	
+
 key_log:
         1 -> s5
 l00005:	1 -> s9
-key_x_to_y:
+key_y_to_x:
 	1 -> s2
 	select rom go to l02010
-	
+
 key_rcl:
-        jsb l00264
-	go to l00376
-	
+        jsb l00124
+	go to l00102
+
 key_sto:
         go to sto
-	
+
 key_rdn:
         go to rdn
 
@@ -49,12 +50,12 @@ key_1_over_x:
 	a + 1 -> a[p]
 l00020:	0 -> b[w]
 	select rom go to asn12
-	
+
 dig6:	a + 1 -> a[x]
 dig5:	a + 1 -> a[x]
 dig4:	a + 1 -> a[x]
 	if no carry go to dig3
-	
+
 key_add:
         jsb l00232
 
@@ -72,16 +73,16 @@ key_mult:
 	0 - c -> c[x]
 l00040:	stack -> a
 	go to l00020
-	
+
 key_pi:
         go to l00164
-	
+
 key_decimal:
-        3 -> p
+        11 -> p
 key_0:	return
-	
+
 	no operation
-	
+
 key_div:
         go to l00040
 
@@ -89,20 +90,20 @@ l00047:	1 -> s5
 key_tan:
         1 -> s1
 	go to key_sqrt
-	
+
 key_cos:
         1 -> s9
 key_sin:
-        go to l00047
-	
+	go to l00047
+
 key_arc:
-	1 -> s10
+        1 -> s10
 	go to l00302
-	
+
 key_sqrt:
         0 -> b[w]
 	select rom go to l01060
-	
+
 rdn:	down rotate
 	go to l00333
 
@@ -113,118 +114,118 @@ dig7:	a + 1 -> a[x]
 
 key_sub:
         go to sub0
-	
-clr:	clear registers
-key_clx:
-        jsb of12
 
-l00071:	go to fst2zx
+clr:	clear registers
+key_cls:
+	jsb of12
+	go to l00335
 
 key_eex:
         go to eex2
-	
+
 key_chs:
         shift right a[w]
 	1 -> s3
-	go to l00166
-	
+	go to l00340
+
 key_enter:
         c -> stack
 l00077:	clear status
 	shift right a[w]
-	jsb fst2zx
-l00102:	a -> b[w]
+	go to l00335
+
+l00102:	m -> c
+	go to l00333
+
+l00104:	a -> b[w]
 	0 -> a[xs]
 	shift left a[ms]
-l00105:	a - 1 -> a[x]
-	if no carry go to l00340
+l00107:	a - 1 -> a[x]
+	if no carry go to l00207
 	if c[xs] = 0
-	     then go to l00346
+	    then go to l00215
 	a exchange b[ms]
 	13 -> p
-	go to l00346
-	
-eex7:	p - 1 -> p
-	c + 1 -> c[x]
-eex8:	if b[p] = 0
-	     then go to eex7
-	1 -> s11
-	shift right a[ms]
-	a exchange c[m]
-	if s4 = 0
-	     then go to den1
-	jsb of14
-	go to fst2zx
-	
-of11:	0 -> c[wp]
-	c - 1 -> c[wp]
-	0 -> c[xs]
-	a + b -> a[x]
-	if no carry go to of13
+	go to l00215
 
-of12:	0 -> c[w]
-of13:	clear status
-	c -> a[w]
-of14:	12 -> p
-	a -> b[x]
-	c -> a[x]
-	if c[xs] = 0
-	     then go to of15
-	0 - c -> c[x]
-	c - 1 -> c[xs]
-	if no carry go to of11
-	5 -> p
-of15:	a exchange c[x]
-	if s4 = 0
-	     then go to l00102
-	a exchange b[x]
-	0 -> b[x]
-	jsb dsp1
-	shift left a[x]
-eex3:	shift right a[w]
-	if p # 12
-	     then go to den7
-	a exchange c[wp]
-	go to eex4
-	
-l00164:	jsb l00264
-	select rom go to l01166
-	
-l00166:	if s4 = 0
-	     then go to chs3
-	a exchange c[wp]
-	0 - c - 1 -> c[xs]
-eex4:	c -> a[w]
-	if c[xs] = 0
-	     then go to eex5
-	0 -> c[xs]
-	0 - c -> c[x]
-eex5:	13 -> p
-eex6:	shift left a[ms]
+l00116:	a exchange b[w]
+	0 -> a[w]
+	if s3 = 0
+	    then go to l00124
+	a - 1 -> a[s]
+	0 - c - 1 -> c[s]
+l00124:	if s7 = 0
+	    then go to l00127
+	c -> stack
+l00127:	1 -> s7
+	0 -> c[w]
 	c - 1 -> c[x]
-	if a[s] >= 1
-	     then go to eex8
-	if a[ms] >= 1
-	     then go to eex6
-	0 -> c[x]
-den1:	jsb dsp1
+	a exchange c[s]
+	if p # 11
+	    then go to l00107
+	jsb l00107
+l00136:	1 -> s6
+l00137:	jsb l00345
 	shift right a[ms]
-den7:	c -> a[s]
-den2:	if p # 12
-	     then go to den4
-	b -> c[w]
+	if p # 12
+	    then go to l00136
+l00143:	b exchange c[w]
 	c + 1 -> c[w]
 	1 -> p
-den3:	shift left a[wp]
+l00146:	shift left a[wp]
 	p + 1 -> p
 	if c[p] = 0
-	     then go to den3
-den4:	a exchange c[w]
+	    then go to l00146
+	c - 1 -> c[w]
+	b exchange c[w]
+	a exchange c[m]
 	if p # 3
-	     then go to den5
-	0 -> c[x]
-	1 -> s6
-	go to eex4
+	    then go to l00257
+l00157:	a exchange c[m]
+l00160:	0 -> a[x]
+	if s4 = 0
+	    then go to l00137
+	go to l00172
+
+l00164:	jsb l00124
+	select rom go to l01166
+
+eex2:	1 -> s4
+	if s11 = 0
+	    then go to dig1
+	shift right a[w]	; eex3 in later ROM
+l00172:	jsb l00345
+	a -> b[x]
+	shift right a[w]
+	a -> b[xs]
+	a - b -> a[x]
+	a exchange c[x]
+	if b[xs] = 0
+	    then go to l00203
+	0 - c -> c[x]
+l00203:	a - c -> c[x]
+	c -> a[x]
+	jsb of14
+	go to l00335
+
+l00207:	shift right a[ms]
+	p - 1 -> p
+	if p # 2
+	    then go to l00107
+	12 -> p
+	0 -> a[w]
+l00215:	0 -> a[ms]
+	a + 1 -> a[p]
+	a + 1 -> a[p]
+	2 -> p
+l00221:	p + 1 -> p
+	a - 1 -> a[p]
+	if no carry go to l00226
+	if b[p] = 0
+	    then go to l00221
+l00226:	a + 1 -> a[p]
+	a exchange b[w]
+	return
 
 sub0:	0 - c - 1 -> c[s]
 l00232:	stack -> a
@@ -234,111 +235,111 @@ l00233:	0 -> b[w]
 	c + 1 -> c[xs]
 	c + 1 -> c[xs]
 	if a >= c[x]
-	     then go to add4
+	    then go to add4
 	a exchange c[w]
 add4:	a exchange c[m]
 	if c[m] = 0
-	     then go to add5
+	    then go to add5
 	a exchange c[w]
 add5:	b exchange c[m]
 add6:	if a >= c[x]
-	     then go to l00276
+	    then go to l00276
 	shift right b[w]
 	a + 1 -> a[x]
 	if b[w] = 0
-	     then go to l00276
+	    then go to l00276
 	go to add6
 
-fst3:	0 -> a[ms]
-	if s3 = 0
-	     then go to l00264
-	a - 1 -> a[s]
-	0 - c - 1 -> c[s]
-l00264:	if s7 = 0
-	     then go to fst5
-	c -> stack
-fst5:	1 -> s7
-	0 -> c[w]
-	c - 1 -> c[w]
-	0 - c -> c[s]
-	c + 1 -> c[s]
-	b exchange c[w]
-	return
+l00257:	c -> a[m]
+	if s6 = 0
+	    then go to l00264
+	p - 1 -> p
+	c - 1 -> c[x]
+l00264:	shift right b[wp]
+	12 -> p
+	if c[m] = 0
+	    then go to l00160
+	c + 1 -> c[x]
+	1 -> s11
+l00272:	if a[p] >= 1
+	    then go to l00157
+	shift left a[m]
+	go to l00272
 
 l00276:	select rom go to add12
 
 l00277:	jsb of12
 	1 -> s5
-	go to fst2zx
+	go to l00335
 
 l00302:	shift right a[w]
 dsp7:	c -> a[s]
-l00304:	0 -> s8
+	0 -> s8
 	go to dsp8
 
 dsp2:	c + 1 -> c[xs]
 dsp3:	1 -> s8
 	if s5 = 0
-	     then go to dsp5
+	    then go to dsp5
 	c + 1 -> c[x]
 	if no carry go to dsp2
 dsp4:	display toggle
 dsp5:	if s0 = 0
-	     then go to dsp3
+	    then go to dsp3
 dsp8:	0 -> s0
 dsp6:	p - 1 -> p
 	if p # 12
-	     then go to dsp6
+	    then go to dsp6
 	display off
 	if s8 = 0
-	     then go to dsp4
+	    then go to dsp4
 	shift left a[w]
 	0 -> s5
 	keys -> rom address
 
 l00331:	c -> stack
 	a exchange c[w]
-l00333:	jsb of13
+l00333:	jsb l00375
 	1 -> s7
-fst2zx:	jsb dsp1
-	jsb fst3
-	go to den2
+l00335:	jsb l00345
+	jsb l00116
+	go to l00143
 
-l00340:	shift right a[ms]
-	p - 1 -> p
-	if p # 2
-	     then go to l00105
-	12 -> p
-	0 -> a[w]
-l00346:	0 -> a[ms]
-	a + 1 -> a[p]
-	a + 1 -> a[p]
-	2 -> p
-l00352:	p + 1 -> p
-	a - 1 -> a[p]
-	if no carry go to l00357
-	if b[p] = 0
-	     then go to l00352
-l00357:	a + 1 -> a[p]
-	a exchange b[w]
-	return
-
-eex2:	1 -> s4
-	if s11 = 0
-	     then go to dig1
-	go to eex3
-
-chs3:	0 - c - 1 -> c[s]
-dsp1:	0 -> s10
+l00340:	if s4 = 0
+	    then go to l00344
+	a + b -> a[xs]
+	if no carry go to l00373
+l00344:	0 - c - 1 -> c[s]
+l00345:	0 -> s10
 	go to dsp7
 
-den5:	if s6 = 0
-	     then go to den6
-	p - 1 -> p
-den6:	shift right b[wp]
-	jsb eex4
-l00376:	m -> c
-	go to l00333
+of11:	a exchange c[xs]
+	0 -> c[wp]
+	c - 1 -> c[wp]
+	0 -> c[xs]
+	if a[xs] >= 1
+	    then go to l00375
+
+of12:	0 -> c[w]
+of13:	clear status
+	c -> a[w]
+of14:	12 -> p
+	if c[xs] = 0
+	    then go to of15
+	0 - c -> c[x]
+	c - 1 -> c[xs]
+	if no carry go to of11
+	5 -> p
+of15:	a exchange c[x]
+	if s4 = 0
+	    then go to l00104
+	a exchange b[x]
+l00373:	0 -> b[x]
+	go to l00172
+
+l00375:	if c[m] >= 1
+	    then go to of13
+	go to of12
 
 ; start of ROM @01
 
@@ -351,10 +352,10 @@ tan15:	a exchange b[w]
 	jsb tnm11
 	stack -> a
 	if s9 = 0
-	     then go to tan16
+	    then go to tan16
 	a exchange c[w]
 tan16:	if s5 = 0
-	     then go to asn12
+	    then go to asn12
 	0 -> c[s]
 	jsb div11
 l01015:	c -> stack
@@ -364,7 +365,7 @@ l01015:	c -> stack
 	stack -> a
 asn12:	jsb div11
 	if s10 = 0
-	     then go to rtn12
+	    then go to rtn12
 atn11:	0 -> a[w]
 	a + 1 -> a[p]
 	a -> b[m]
@@ -372,7 +373,7 @@ atn11:	0 -> a[w]
 atn12:	c - 1 -> c[x]
 	shift right b[wp]
 	if c[xs] = 0
-	     then go to atn12
+	    then go to atn12
 atn13:	shift right a[wp]
 	c + 1 -> c[x]
 	if no carry go to atn13
@@ -389,7 +390,7 @@ sqt11:	b exchange c[w]
 tnm11:	c -> stack
 	a exchange c[w]
 	if c[p] = 0
-	     then go to tnm12
+	    then go to tnm12
 	0 - c -> c[w]
 tnm12:	c -> a[w]
 	b -> c[x]
@@ -397,11 +398,11 @@ tnm12:	c -> a[w]
 
 l01060:	c -> a[w]
 	if s1 = 0
-	     then go to sqt11
+	    then go to sqt11
 	if s10 = 0
-	     then go to l01155
+	    then go to l01155
 	if s5 = 0
-	     then go to atn11
+	    then go to atn11
 	0 - c - 1 -> c[s]
 	a exchange c[s]
 	go to l01015
@@ -427,8 +428,8 @@ atn18:	a -> b[w]
 	if no carry go to atn14
 	0 -> c[w]
 	0 -> b[x]
-	shift right a[ms]
-	jsb div14
+	c + 1 -> c[p]
+	jsb l01267
 	c - 1 -> c[p]
 	stack -> a
 	a exchange c[w]
@@ -453,7 +454,7 @@ atn18:	a -> b[w]
 	c + c -> c[w]
 	jsb div11
 	if s9 = 0
-	     then go to l01154
+	    then go to l01154
 	0 - c - 1 -> c[s]
 	jsb add10
 l01154:	0 -> s1
@@ -461,7 +462,7 @@ l01155:	0 -> c[w]
 	c - 1 -> c[p]
 	c + 1 -> c[x]
 	if s1 = 0
-	     then go to mpy11
+	    then go to mpy11
 	jsb div11
 	jsb atc1
 	c + c -> c[w]
@@ -499,7 +500,7 @@ atcd1:	6 -> p
 	load constant 4
 	load constant 9
 rtn11:	if s1 = 0
-	     then go to rtn12
+	    then go to rtn12
 	return
 
 add10:	0 -> a[w]
@@ -517,7 +518,7 @@ pqo15:	c + 1 -> c[s]
 pqo16:	a - b -> a[w]
 	if no carry go to pqo15
 	a + b -> a[w]
-pqo13:	select rom go to l02245	; -> pqo23
+pqo13:	select rom go to l02245
 
 mpy11:	select rom go to mpy21
 
@@ -532,7 +533,7 @@ sqt16:	a - c -> a[w]
 	p - 1 -> p
 sqt17:	shift right c[wp]
 	if p # 0
-	     then go to sqt16
+	    then go to sqt16
 	go to tnm12
 
 div14:	c + 1 -> c[p]
@@ -540,9 +541,9 @@ div15:	a - b -> a[ms]
 	if no carry go to div14
 	a + b -> a[ms]
 	shift left a[ms]
-	p - 1 -> p
+l01267:	p - 1 -> p
 	if p # 0
-l01271:	     then go to div15
+l01271:	    then go to div15
 	go to tnm12
 
 sqt12:	p - 1 -> p
@@ -550,16 +551,16 @@ sqt12:	p - 1 -> p
 	if no carry go to sqt18
 	select rom go to l00277
 
-add12:	c - 1 -> c[xs]		; (from rom 00276)
+add12:	c - 1 -> c[xs]
 	c - 1 -> c[xs]
 	0 -> a[x]
 	a - c -> a[s]
 	if a[s] >= 1
-	     then go to add13
-	select rom go to l02306	; -> mpy26
+	    then go to add13
+	select rom go to l02306
 
 add13:	if a >= b[m]
-	     then go to add14
+	    then go to add14
 	0 - c - 1 -> c[s]
 	a exchange b[w]
 add14:	a - b -> a[w]
@@ -587,11 +588,11 @@ sqt18:	a + b -> a[x]
 	c - 1 -> c[p]
 sqt14:	c + 1 -> c[s]
 	if p # 0
-	     then go to sqt12
+	    then go to sqt12
 	a exchange c[x]
 	0 -> a[x]
 	if c[p] >= 1
-	     then go to sqt13
+	    then go to sqt13
 	shift right a[w]
 sqt13:	shift right c[w]
 	b exchange c[x]
@@ -614,7 +615,7 @@ tan13:	b -> c[w]
 	a exchange c[wp]
 	stack -> a
 	if b[s] = 0
-	     then go to tan15
+	    then go to tan15
 	shift left a[w]
 tan14:	a exchange c[wp]
 	c -> stack
@@ -625,19 +626,19 @@ tan14:	a exchange c[wp]
 
 ; start of ROM @02
 
-err21:	select rom go to l00001	; -> l00277
+err21:	select rom go to l00001
 
 ln24:	a exchange b[s]
 	a + 1 -> a[s]
 	shift right c[ms]
 	shift left a[wp]
 	go to ln26
-	
+
 xty22:	stack -> a
 	jsb mpy21
 l02010:	c -> a[w]
 	if s8 = 0
-	     then go to exp21
+	    then go to exp21
 	0 -> a[w]
 	a - c -> a[m]
 	if no carry go to err21
@@ -650,6 +651,7 @@ ln26:	a -> b[w]
 	a - 1 -> a[p]
 	if no carry go to ln25
 	a exchange b[wp]
+	a -> b[s]
 	a + b -> a[s]
 	if no carry go to ln24
 	7 -> p
@@ -672,30 +674,31 @@ ln26:	a -> b[w]
 	a exchange c[w]
 	a - c -> c[w]
 	if b[xs] = 0
-	     then go to ln27
+	    then go to ln27
 	a - c -> c[w]
 ln27:	a exchange b[w]
 ln28:	p - 1 -> p
 	shift left a[w]
 	if p # 1
-	     then go to ln28
+	    then go to ln28
 	a exchange c[w]
 	if c[s] = 0
-	     then go to ln29
+	    then go to ln29
 	0 - c - 1 -> c[m]
 ln29:	c + 1 -> c[x]
 	11 -> p
 	jsb mpy27
 	if s9 = 0
-	     then go to xty22
+	    then go to xty22
 	if s5 = 0
-	     then go to rtn21
+	    then go to l02224
 	jsb lnc10
 	jsb mpy22
-	go to rtn21
+	go to l02224
 
 exp21:	jsb lnc10
 	jsb pre21
+	0 -> b[ms]
 	jsb lnc2
 	11 -> p
 	jsb pqo21
@@ -710,8 +713,6 @@ exp21:	jsb lnc10
 	jsb pqo21
 	jsb pqo21
 	jsb pqo21
-	6 -> p
-	0 -> a[wp]
 	13 -> p
 	b exchange c[w]
 	a exchange c[w]
@@ -719,10 +720,10 @@ exp21:	jsb lnc10
 	go to exp23
 
 pre23:	if s2 = 0
-	     then go to pre24
+	    then go to pre24
 	a + 1 -> a[x]
 pre29:	if a[xs] >= 1
-	     then go to pre27
+	    then go to pre27
 pre24:	a - b -> a[ms]
 	if no carry go to pre23
 	a + b -> a[ms]
@@ -733,26 +734,26 @@ pre25:	shift right a[w]
 	0 -> c[wp]
 	a exchange c[x]
 pre26:	if c[s] = 0
-	     then go to pre28
+	    then go to pre28
 	a exchange b[w]
 	a - b -> a[w]
-	0 - c - 1 -> c[w]
+	0 - c - 1 -> c[x]
 pre28:	shift right a[w]
 pqo23:	b exchange c[w]
 	0 -> c[w]
 	c - 1 -> c[m]
 	if s2 = 0
-	     then go to pqo28
+	    then go to pqo28
 	load constant 4
 	c + 1 -> c[m]
 	if no carry go to pqo24
 pqo27:	load constant 6
 pqo28:	if p # 1
-	     then go to pqo27
+	    then go to pqo27
 	shift right c[w]
 pqo24:	shift right c[w]
 nrm26:	if s2 = 0
-	     then go to rtn21
+	    then go to l02224
 	return
 
 lncd2:	7 -> p
@@ -776,16 +777,16 @@ exp22:	a -> b[w]
 exp23:	a exchange c[w]
 	a - 1 -> a[s]
 	if no carry go to exp22
-	a exchange b[w]
+	0 -> a[w]
 	a + 1 -> a[p]
-	jsb nrm21
-rtn21:	select rom go to rtn11
+	jsb mpy26
+l02224:	select rom go to rtn11
 
 eca21:	shift right a[wp]
 eca22:	a - 1 -> a[s]
 	if no carry go to eca21
-	0 -> a[s]
-	a + b -> a[w]
+	a + b -> a[wp]
+	a exchange b[s]
 	return
 
 pqo21:	select rom go to pqo11
@@ -810,11 +811,11 @@ l02250:	a - c -> c[s]
 div22:	a exchange b[m]
 	0 -> a[w]
 	if p # 12
-	     then go to mpy27
+	    then go to mpy27
 	if c[m] >= 1
-	     then go to div23
+	    then go to div23
 	if s1 = 0
-	     then go to err21
+	    then go to err21
 	b -> c[wp]
 	a - 1 -> a[m]
 	c + 1 -> c[xs]
@@ -834,28 +835,28 @@ lnc2:	0 -> s8
 
 pre27:	a + 1 -> a[m]
 	if no carry go to pre25
-myp26:	a + b -> a[w]
+mpy26:	a + b -> a[w]
 mpy27:	c - 1 -> c[p]
-l02306:	if no carry go to myp26
+l02306:	if no carry go to mpy26
 mpy28:	shift right a[w]
 	p + 1 -> p
 	if p # 13
-	     then go to mpy27
+	    then go to mpy27
 	c + 1 -> c[x]
 nrm21:	0 -> a[s]
 	12 -> p
 	0 -> b[w]
 nrm23:	if a[p] >= 1
-	     then go to nrm24
+	    then go to nrm24
 	shift left a[w]
 	c - 1 -> c[x]
 	if a[w] >= 1
-	     then go to nrm23
+	    then go to nrm23
 	0 -> c[w]
 nrm24:	a -> b[x]
 	a + b -> a[w]
 	if a[s] >= 1
-	     then go to mpy28
+	    then go to mpy28
 	a exchange c[m]
 	c -> a[w]
 	0 -> b[w]
@@ -898,5 +899,3 @@ lnc10:	0 -> c[w]
 
 lncd3:	5 -> p
 	go to lnc6
-
-	.symtab
