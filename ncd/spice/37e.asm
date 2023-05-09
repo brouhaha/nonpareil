brouhaha/nonpareil

commit 3ae3d634b076b9a6e74ee60813a74f6536ecb4b8

Spice: document factorial routines, and 37e/38e/38c key dispatch.

ncd/spice/37e.asm
@@ -79,7 +79,7 @@ L02064:  0 -> c[w]
 L02067:  0 -> b[w]
          b exchange c[m]
          a exchange c[w]
-L02072:  if 0 = s 11
+L02072:  if s 11 = 0
            then go to L03506
          jsb S02124
          jsb S02126
@@ -94,7 +94,7 @@ S02103:  0 -> s 3
          0 -> s 13
          0 -> s 14
          1 -> s 11
-         if 0 = s 3
+         if s 3 = 0
            then go to L02113
          0 -> s 11
 L02113:  0 -> c[w]
@@ -158,7 +158,7 @@ L02161:  jsb S02102
            then go to L02135
 L02202:  delayed rom @02
          jsb S01062
-         if 0 = s 10
+         if s 10 = 0
            then go to L02211
          a exchange c[w]
          0 - c - 1 -> c[s]
@@ -391,10 +391,10 @@ L02516:  register -> c 7
          jsb S02661
          jsb S02716
          register -> c 6
-         if 0 = s 12
+         if s 12 = 0
            then go to L02573
          jsb S02661
-L02543:  if 0 = s 12
+L02543:  if s 12 = 0
            then go to L02574
          if c[s] # 0
            then go to L02550
@@ -456,7 +456,7 @@ S02631:  c -> register 6
 S02632:  if s 14 = 1
            then go to L02646
 L02634:  register -> c 4
-         if 0 = s 11
+         if s 11 = 0
            then go to L02651
 S02637:  c -> a[w]
          0 -> b[w]
@@ -467,7 +467,7 @@ S02643:  c -> register 5
 S02644:  if s 14 = 1
            then go to L02634
 L02646:  register -> c 2
-         if 0 = s 11
+         if s 11 = 0
            then go to S02637
 L02651:  a exchange c[w]
          register -> c 3
@@ -569,60 +569,62 @@ L02776:  jsb S02637
          1 -> s 1
          go to L03070
 
-L03001:  if c[s] # 0
+
+; factorial
+xft100: if c[s] # 0
            then go to L03063
          if c[xs] # 0
            then go to L03063
          c -> a[w]
-L03006:  a -> b[w]
+xft110:  a -> b[w]
          shift left a[ms]
          if a[wp] # 0
-           then go to L03061
+           then go to xft120
          a + 1 -> a[x]
          if a >= c[x]
-           then go to L03017
+           then go to xfgt130
          c + 1 -> c[xs]
          if n/c go to L03066
-L03017:  0 -> c[w]
+xft130:  0 -> c[w]
          c + 1 -> c[p]
          shift right c[w]
          c + 1 -> c[s]
          b exchange c[w]
-L03024:  if b[p] = 0
-           then go to L03030
+xft140:  if b[p] = 0
+           then go to xft150
          shift right b[wp]
          c + 1 -> c[x]
-L03030:  0 -> a[w]
+xft150:  0 -> a[w]
          a - c -> a[p]
-         if n/c go to L03036
+         if n/c go to xft170
          shift left a[w]
-L03034:  a + b -> a[w]
-         if n/c go to L03034
-L03036:  a - c -> a[s]
-         if n/c go to L03045
+xft160:  a + b -> a[w]
+         if n/c go to xft160
+xft170:  a - c -> a[s]
+         if n/c go to xft190
          shift right a[wp]
          a + 1 -> a[w]
          c + 1 -> c[x]
-L03043:  a + b -> a[w]
-         if n/c go to L03043
-L03045:  a exchange b[wp]
+xft180:  a + b -> a[w]
+         if n/c go to xft180
+xft190:  a exchange b[wp]
          c - 1 -> c[p]
-         if n/c go to L03024
+         if n/c go to xft140
          c - 1 -> c[s]
-         if n/c go to L03024
+         if n/c go to xft140
          shift left a[w]
          a -> b[x]
          0 -> c[ms]
          a + b -> a[wp]
          a + c -> a[w]
          a exchange c[ms]
-         go to L03066
+         go to L03066		; in some othe rmodels, return instruction
 
-L03061:  a - 1 -> a[x]
-         if n/c go to L03006
+xft120:  a - 1 -> a[x]
+         if n/c go to xft110
 L03063:  p <- 7
          delayed rom @00
-         go to L00340
+         go to L00340		; err0?
 
 L03066:  delayed rom @00
          go to L00020
@@ -690,7 +692,7 @@ L03153:  jsb S03257
          jsb S03266
          jsb S03313
          jsb S03254
-         if 0 = s 12
+         if s 12 = 0
            then go to L03174
          jsb S03264
          jsb S03262
@@ -721,9 +723,9 @@ L03211:  jsb S03310
          c + 1 -> c[xs]
          if c[xs] # 0
            then go to L03233
-L03225:  if 0 = s 8
+L03225:  if s 8 = 0
            then go to L03070
-L03227:  if 0 = s 14
+L03227:  if s 14 = 0
            then go to L03233
          delayed rom @01
          jsb S00677
@@ -1177,32 +1179,32 @@ L04046:  delayed rom @00
 L04056:  0 -> c[w]
          go to L04170
 
-         a + 1 -> a[x]
-         a + 1 -> a[x]
-         a + 1 -> a[x]
-         a + 1 -> a[x]
-         0 -> c[x]
+         a + 1 -> a[x]          ; key 15 - FV      y^x
+         a + 1 -> a[x]          ; key 14 - PMT     sqrt
+         a + 1 -> a[x]          ; key 13 - PV      1/x
+         a + 1 -> a[x]          ; key 12 - i       12/
+L04064:  0 -> c[x]		; key 11 - n       12x
          jsb S04342
          if s 11 = 1
            then go to L04202
          shift left a[x]
          if s 13 = 1
            then go to L04500
-         if 0 = s 14
+         if s 14 = 0
            then go to L04123
          if a[x] # 0
            then go to L04255
          go to L04265
 
-         go to L04330
+         go to L04330		; key 74 - Sigma+  Sigma-
 
-         go to L04347
+         go to L04347		; key 73 - .
 
-         go to L04020
+         go to L04020		; key 72 - 0
 
-         if s 13 = 1
+         if s 13 = 1		; key 71 - /       n!
            then go to L04737
-         if 0 = s 12
+         if s 12 = 0
            then go to L04431
          p - 1 -> p
 L04110:  p - 1 -> p
@@ -1218,7 +1220,7 @@ L04116:  if s 13 = 1
          go to L04420
 
 L04122:  a + 1 -> a[p]
-L04123:  if 0 = s 1
+L04123:  if s 1 = 0
            then go to L04013
          delayed rom @04
          a -> rom address
@@ -1233,16 +1235,17 @@ L04131:  if s 10 = 1
          if s 12 = 1
            then go to L04731
          a + 1 -> a[x]
-         a + 1 -> a[x]
-         a + 1 -> a[x]
-         if n/c go to L04317
-         if s 13 = 1
+         a + 1 -> a[x]		; key 54 - 6
+         a + 1 -> a[x]          ; key 53 - 5
+         if n/c go to L04317	; key 52 - 4
+
+         if s 13 = 1		; key 51 - +       std dev
            then go to L04733
-         if 0 = s 12
+         if s 12 = 0
            then go to L04511
          go to L04112
 
-L04150:  if 0 = s 13
+L04150:  if s 13 = 0
            then go to L04056
          delayed rom @00
          go to L00011
@@ -1252,16 +1255,16 @@ L04154:  if s 13 = 1
          delayed rom @11
          go to L04523
 
-         go to L04150
+         go to L04150		; key 34 - CLx     CL ALL
 
-         go to L04273
+         go to L04273		; key 33 - x<>y    Rdn
 
-         go to L04301
+         go to L04301		; key 32 - CHS     CL FIN
 
-         if s 13 = 1
+         if s 13 = 1            ; key 31 - ENTER^  AMORT
            then go to L04310
          if s 12 = 1
-           then go to L04543
+           then go to L04543	; STO ENTER^ self test
          c -> stack
 L04170:  1 -> s 0
 L04171:  delayed rom @00
@@ -1293,15 +1296,15 @@ L04215:  jsb S04173
          delayed rom @00
          go to L00250
 
-         go to L04250
+         go to L04250		; key 25 - f
 
-         go to L04154
+         go to L04154		; key 24 - %T      PRICE
 
-         go to L04116
+         go to L04116		; key 23 - %       Delta%
 
-         go to L04312
+         go to L04312		; key 22 - RCL     LN
 
-         if s 13 = 1
+         if s 13 = 1		; key 21 - STO     e^x
            then go to L04504
          0 -> s 1
          jsb S04173
@@ -1315,16 +1318,17 @@ L04232:  if p # 10
          go to L04044
 
 L04237:  a + 1 -> a[x]
-         a + 1 -> a[x]
-         a + 1 -> a[x]
-         if n/c go to L04131
-         if s 13 = 1
+         a + 1 -> a[x]		; key 44 - 9
+         a + 1 -> a[x]		; key 43 - 8
+         if n/c go to L04131	; key 42 - 7
+
+         if s 13 = 1		; key 41 - -       mean
            then go to L04436
-         if 0 = s 12
+         if s 12 = 0
            then go to L04510
          go to L04111
 
-L04250:  if s 11 = 1
+L04250:  if s 11 = 1		; key 25 - f
            then go to L04212
          jsb S04173
          1 -> s 13
@@ -1372,12 +1376,13 @@ L04312:  if s 13 = 1
          go to L04114
 
 L04317:  a + 1 -> a[x]
-         a + 1 -> a[x]
-         a + 1 -> a[x]
-         if n/c go to L04017
-         if s 13 = 1
+         a + 1 -> a[x]		; key 64 - 3
+         a + 1 -> a[x]		; key 63 - 2
+         if n/c go to L04017	; key 62 - 1
+
+         if s 13 = 1		; key 61 - /       n!
            then go to L04735
-         if 0 = s 12
+         if s 12 = 0
            then go to L04515
          go to L04110
 
@@ -1497,7 +1502,7 @@ L04464:  y -> a
          delayed rom @02
          jsb S01167
          stack -> a
-         if 0 = s 7
+         if s 7 = 0
            then go to L04476
          0 - c - 1 -> c[s]
 L04476:  delayed rom @00
@@ -1546,7 +1551,9 @@ L04534:  y -> a
          a exchange c[w]
          go to L04525
 
-L04543:  binary
+
+; STO ENTER^ self test
+         L04543:  binary
          clear regs
          c + 1 -> c[x]
          m2 exchange c
@@ -1663,7 +1670,7 @@ L04671:  c -> a[w]
          jsb S04741
          m2 exchange c
          m2 -> c
-         if 0 = s 8
+         if s 8 = 0
            then go to L04170
 L04726:  p <- 6
 L04727:  m2 exchange c
@@ -1679,11 +1686,11 @@ L04735:  delayed rom @07
          go to L03525
 
 L04737:  delayed rom @06
-         go to L03001
+         go to xft100		; factorial
 
 S04741:  0 -> b[w]
          a exchange b[m]
-S04743:  if 0 = s 13
+S04743:  if s 13 = 0
            then go to L04750
          a exchange c[w]
          0 - c - 1 -> c[s]

ncd/spice/1820-2162.asm
@@ -862,24 +862,26 @@ S01403:  select rom go to x-mp2-10
          select rom go to x-mp2-13
 S01406:  select rom go to x-dv2-10
 
-S01407:  p <- 12
+
+; factorial
+xft100:  p <- 12		; this instrunction not in 41C
          if c[s] # 0
            then go to err0
          if c[xs] # 0
            then go to err0
          c -> a[w]
-L01415:  a -> b[w]
+xft110:  a -> b[w]
          shift left a[ms]
          if a[wp] # 0
-           then go to L01426
+           then go to xft120
          a + 1 -> a[x]
          if a >= c[x]
            then go to xft130
          c + 1 -> c[xs]
          return
 
-L01426:  a - 1 -> a[x]
-         if n/c go to L01415
+xft120:  a - 1 -> a[x]
+         if n/c go to xft110
          delayed rom @00
          go to err0
 
@@ -899,13 +901,13 @@ xft150:  0 -> a[w]
 xft160:  a + b -> a[w]
          if n/c go to xft160
 xft170:  a - c -> a[s]
-         if n/c go to xfg190
+         if n/c go to xft190
          shift right a[wp]
          a + 1 -> a[w]
          c + 1 -> c[x]
 xft180:  a + b -> a[w]
          if n/c go to xft180
-xfg190:  a exchange b[wp]
+xft190:  a exchange b[wp]
          c - 1 -> c[p]
          if n/c go to xft140
          c - 1 -> c[s]

ncd/spice/1820-2162.inc
@@ -58,7 +58,7 @@ S01044  .equ @01044
 S01151	.equ @01151
 L01331	.equ @01331
 S01377  .equ @01377
-S01407  .equ @01407
+xft100  .equ @01407	; factorial
 L01474  .equ @01474
 L01476	.equ @01476
 L01545	.equ @01545

ncd/spice/32e.asm
@@ -708,8 +708,8 @@ L03035:  0 -> a[w]		; g-shifted key 73 - 10^x
          jsb S01044
          go to L03020
 
-L03045:  delayed rom @03	; g-shifted key 25 - n!
-         jsb S01407
+L03045:  delayed rom @03	; g-shifted key 25 - factorial, n!
+         jsb xft100		; factorial
          go to L03020
 
 S03050:  0 -> a[w]
@@ -853,7 +853,7 @@ L03213:  a - c -> a[x]
 L03216:  delayed rom @13	; g-shifted key 63 - COSH-1
          go to L05424
 
-         go to L03045		; g-shifted key 25 - n!
+         go to L03045		; g-shifted key 25 - factorial, n!
 
          go to L03230		; g-shifted key 24 - std dev
 

ncd/spice/34c.asm
@@ -2675,7 +2675,7 @@ L06573:  if c[xs] # 0
          if a[w] # 0
            then go to L06630
          delayed rom @03
-         jsb S01407
+         jsb xft100		; factorial
 L06610:  delayed rom @04
          go to L02002
 

ncd/spice/37e.ncd.tmpl
@@ -76,42 +76,43 @@ SPDX-License-Identifier: GPL-3.0-only
       </switch_pos>
     </switch>
 
-    <key user_keycode="11" hw_keycode="0064"/>
-    <key user_keycode="12" hw_keycode="0063"/>
-    <key user_keycode="13" hw_keycode="0062"/>
-    <key user_keycode="14" hw_keycode="0061"/>
-    <key user_keycode="15" hw_keycode="0060"/>
+                                               <!-- unshifted  f-shifted -->
+    <key user_keycode="11" hw_keycode="0064"/> <!-- n          12x       -->
+    <key user_keycode="12" hw_keycode="0063"/> <!-- i          12/       -->
+    <key user_keycode="13" hw_keycode="0062"/> <!-- PV         1/x       -->
+    <key user_keycode="14" hw_keycode="0061"/> <!-- PMT        sqrt      -->
+    <key user_keycode="15" hw_keycode="0060"/> <!-- FV         y^x       -->
 
-    <key user_keycode="21" hw_keycode="0224"/>
-    <key user_keycode="22" hw_keycode="0223"/>
-    <key user_keycode="23" hw_keycode="0222"/>
-    <key user_keycode="24" hw_keycode="0221"/>
-    <key user_keycode="25" hw_keycode="0220"/>
+    <key user_keycode="21" hw_keycode="0224"/> <!-- STO        e^x       -->
+    <key user_keycode="22" hw_keycode="0223"/> <!-- RCL        LN        -->
+    <key user_keycode="23" hw_keycode="0222"/> <!-- %          Delta%    -->
+    <key user_keycode="24" hw_keycode="0221"/> <!-- %T         PRICE     -->
+    <key user_keycode="25" hw_keycode="0220"/> <!-- f                    -->
 
-    <key user_keycode="31" hw_keycode="0163"/>
-    <key user_keycode="32" hw_keycode="0162"/>
-    <key user_keycode="33" hw_keycode="0161"/>
-    <key user_keycode="34" hw_keycode="0160"/>
+    <key user_keycode="31" hw_keycode="0163"/> <!-- ENTER^     AMORT     -->
+    <key user_keycode="32" hw_keycode="0162"/> <!-- CHS        CL FIN    -->
+    <key user_keycode="33" hw_keycode="0161"/> <!-- x<>y       Rdn       -->
+    <key user_keycode="34" hw_keycode="0160"/> <!-- CLx        CL ALL    -->
 
-    <key user_keycode="41" hw_keycode="0243"/>
-    <key user_keycode="42" hw_keycode="0242"/>
-    <key user_keycode="43" hw_keycode="0241"/>
-    <key user_keycode="44" hw_keycode="0240"/>
+    <key user_keycode="41" hw_keycode="0243"/> <!-- -          mean      -->
+    <key user_keycode="42" hw_keycode="0242"/> <!-- 7                    -->
+    <key user_keycode="43" hw_keycode="0241"/> <!-- 8                    -->
+    <key user_keycode="44" hw_keycode="0240"/> <!-- 9                    -->
 
-    <key user_keycode="51" hw_keycode="0143"/>
-    <key user_keycode="52" hw_keycode="0142"/>
-    <key user_keycode="53" hw_keycode="0141"/>
-    <key user_keycode="54" hw_keycode="0140"/>
+    <key user_keycode="51" hw_keycode="0143"/> <!-- +          std dev   -->
+    <key user_keycode="52" hw_keycode="0142"/> <!-- 4                    -->
+    <key user_keycode="53" hw_keycode="0141"/> <!-- 5                    -->
+    <key user_keycode="54" hw_keycode="0140"/> <!-- 6                    -->
 
-    <key user_keycode="61" hw_keycode="0323"/>
-    <key user_keycode="62" hw_keycode="0322"/>
-    <key user_keycode="63" hw_keycode="0321"/>
-    <key user_keycode="64" hw_keycode="0320"/>
+    <key user_keycode="61" hw_keycode="0323"/> <!-- *          lin est   -->
+    <key user_keycode="62" hw_keycode="0322"/> <!-- 1                    -->
+    <key user_keycode="63" hw_keycode="0321"/> <!-- 2                    -->
+    <key user_keycode="64" hw_keycode="0320"/> <!-- 3                    -->
 
-    <key user_keycode="71" hw_keycode="0103"/>
-    <key user_keycode="72" hw_keycode="0102"/>
-    <key user_keycode="73" hw_keycode="0101"/>
-    <key user_keycode="74" hw_keycode="0100"/>
+    <key user_keycode="71" hw_keycode="0103"/> <!-- /          n!        -->
+    <key user_keycode="72" hw_keycode="0102"/> <!-- 0                    -->
+    <key user_keycode="73" hw_keycode="0101"/> <!-- .                    -->
+    <key user_keycode="74" hw_keycode="0100"/> <!-- Sigma+     Sigma-    -->
   </keyboard>
 
   <obj_file addr_space="inst">

ncd/spice/38c.asm
@@ -42,7 +42,7 @@ L02013:  bank toggle
 
 S02020:  bank toggle
 
-         delayed rom @15
+         delayed rom @15	; self-test
          go to L06717
 
 L02023:  bank toggle
@@ -88,9 +88,10 @@ L02053:  p <- 2
          p <- 1
          shift left a[x]
          if a >= c[xs]
-           then go to L02076
+           then go to L02076	; high digit >9
          if a >= c[p]
-           then go to L03017
+           then go to L03017	; low digit > 9
+; 00-99: GTO
          if 0 = s 0
            then go to L03230
          p <- 9
@@ -168,8 +169,12 @@ L02164:  p <- 4
 L02171:  delayed rom @05
          a -> rom address
 
+; 0xa <= low digit <= 0xe (a[1] is at +1)
+; on entry:
+;   p=1
 L02173:  a + 1 -> a[p]
-         if n/c go to L02204
+         if n/c go to L02204	; 0xa <= low digit <= 0xd (a[1] is at +2)
+; 0x[0-9]e: RCL .[0-9]
          p <- 6
          load constant 2
          load constant 2
@@ -178,8 +183,11 @@ L02173:  a + 1 -> a[p]
          jsb S02353
          go to L02110
 
+; 0xa <= low digit <= 0xd (a[1] is at +2)
+; on entry:
+;   p=1
 L02204:  a + 1 -> a[p]
-         if n/c go to L02214
+         if n/c go to L02214	; 0xa <= low digit <= 0xc (a[1] is at +3)
          p <- 6
          load constant 2
          load constant 4
@@ -187,8 +195,11 @@ L02204:  a + 1 -> a[p]
            then go to L02565
          go to L02347
 
+; 0xa <= low digit <= 0xc (a[1] is at +3)
+; on entry:
+;   p=1
 L02214:  a + 1 -> a[p]
-         if n/c go to L02245
+         if n/c go to L02245	; 0xa <= low digit <= 0xb (a[1] is at +4)
          p <- 6
          jsb S02340
          if s 0 = 1
@@ -196,6 +207,7 @@ L02214:  a + 1 -> a[p]
          jsb S02353
          go to L02125
 
+; low digit == 0xa (a[1] is at +5)
 L02224:  p <- 2
          load constant 5
          a - c -> a[xs]
@@ -214,14 +226,18 @@ L02241:  c + 1 -> c[p]
          if n/c go to L02241
          go to L02031
 
+; 0xa <= low digit <= 0xb (a[1] is at +4)
+; on entry:
+;   p=1
 L02245:  a + 1 -> a[p]
-         if n/c go to L02224
-         a - 1 -> a[p]
+         if n/c go to L02224	; low digit == 0xa (a[1] is at +5)
+         a - 1 -> a[p]		; a[1] is at +4
          p <- 2
-         load constant 7
+         load constant 7	; is high digit >= 7?
          if a >= c[xs]
-           then go to L02164
-         p <- 6
+           then go to L02164	; yes, not a "g" prefix
+; g-shifted digits 0-7
+         p <- 6			; load "g" keycode
          load constant 2
          load constant 5
          if s 0 = 1
@@ -386,7 +402,7 @@ L02461:  0 -> c[w]
          go to L02513
 
 L02463:  delayed rom @03
-         jsb S01407
+         jsb xft100		; factorial
          go to L02601
 
 L02466:  delayed rom @06
@@ -663,9 +679,9 @@ L02740:  y -> a
 
          go to L02474
 
-         go to L02463
+         go to L02463		; 3b factorial
 
-         go to L02404
+         go to L02404		; 4b pause
 
          go to L02662
 
@@ -704,9 +720,13 @@ L03012:  clear status
          delayed rom @04
          go to L02127
 
+; high digit < 9, low digit > 9
+; on entry:
+;   p=1
 L03017:  a + 1 -> a[p]
          if a[p] # 0
-           then go to L02173
+           then go to L02173	; 0xa <= low digit <= 0xe (a[1] is at +1)
+; 0x[0-9]f: digit
          if s 0 = 1
            then go to L02347
          if 0 = s 7
@@ -2841,6 +2861,8 @@ L06714:  load constant 7
          p <- 12
          go to L06644
 
+
+; STO ENTER^ self-test
 L06717:  clear regs
          c + 1 -> c[w]
          m2 exchange c
@@ -3470,7 +3492,7 @@ L12015:  binary
 
 L12017:  return
 
-L12020:  bank toggle
+L12020:  bank toggle	; self-test
 
          jsb S12000
          go to L12006
@@ -3762,7 +3784,7 @@ L12416:  if p = 10
          go to L12633
 
 L12424:  a + 1 -> a[x]
-L12425:  if p = 5
+L12425:  if p = 5		; key 72 (@102) - 0, g mean
            then go to L12444
          if p = 1
            then go to L12660
@@ -3793,21 +3815,21 @@ L12454:  a - 1 -> a[p]
            then go to L12023
          go to L12445
 
-         go to L12472
+         go to L12472		; key 15 (@060) - FV, f IRR, Nj
 
-         go to L12664
+         go to L12664		; key 14 (@061) - PMT, f RND, g CFj
 
-         go to L12701
+         go to L12701		; key 13 (@062) - PV, f NPV, g CF0
 
-         a + 1 -> a[x]
-         nop
+         a + 1 -> a[x]		; key 12 (@063) - i, f INT, g 12/
+         nop			; key 11 (@064) - n, f AMORT, 12x
          nop
          if p # 2
            then go to L12703
          p <- 8
          go to L12534
 
-L12472:  if p # 2
+L12472:  if p # 2		; key 15 (@060) - FV, f IRR, Nj
            then go to L12677
          p <- 10
          go to L12574
@@ -3815,13 +3837,13 @@ L12472:  if p # 2
 L12476:  p <- 10
          go to L12633
 
-         go to L12752
+         go to L12752		; key 74 (@100) - R/S, f Sigma+, g Sigma-
 
-         go to L12742
+         go to L12742		; key 73 (@101) - ., g std dev
 
-         go to L12425
+         go to L12425		; key 72 (@102) - 0, g mean
 
-         if p = 8
+         if p = 8		; key 71 (@103) - /, f 1/x, g weighted
            then go to L12670
          if p = 10
            then go to L12516
@@ -3855,10 +3877,11 @@ L12534:  a + 1 -> a[x]
 L12535:  a + 1 -> a[x]
          if p = 4
            then go to L12203
-         a + 1 -> a[x]
-L12541:  a + 1 -> a[x]
-         if n/c go to L12717
-         if p = 8
+         a + 1 -> a[x]		; key 54 (@140) - 6, g x=0
+L12541:  a + 1 -> a[x]		; key 53 (@141) - 5, g x<y
+         if n/c go to L12717	; key 52 (@142) - 4, g PSE
+
+         if p = 8		; key 51 (@143) - +, f DATE, g MEM
            then go to L13670
          if p = 10
            then go to L12737
@@ -3873,14 +3896,14 @@ L12554:  p <- 1
 L12556:  load constant 8
          go to L12514
 
-         go to L12775
+         go to L12775		; key 34 (@160) - CLx, f CLR ALL, g CLP
 
-         go to L12575
+         go to L12575		; key 33 (@161) - x<>y, f CLR Sigma, g Rdn
 
-         go to L12576
+         go to L12576		; key 32 (@162) - CHS, f CLR FIN, g EEX
 
-         if p = 9
-           then go to L12020
+         if p = 9		; key 31 (@163) - ENTER^, f CLR PREFIX, g LASTx
+           then go to L12020	; self-test
          if p # 10
            then go to L12577
          if 0 = s 0
@@ -3890,8 +3913,8 @@ L12571:  p <- 12
 
 L12573:  a + 1 -> a[x]
 L12574:  a + 1 -> a[x]
-L12575:  a + 1 -> a[x]
-L12576:  a + 1 -> a[x]
+L12575:  a + 1 -> a[x]		; key 33 (@161) - x<>y, f CLR Sigma, g Rdn
+L12576:  a + 1 -> a[x]		; key 32 (@162) - CHS, f CLR FIN, g EEX
 L12577:  a + 1 -> a[x]
 L12600:  if p = 10
            then go to L12605
@@ -3960,7 +3983,8 @@ L12660:  if a[p] # 0
            then go to L12454
          a + 1 -> a[x]
          if n/c go to L12413
-L12664:  if p # 2
+
+L12664:  if p # 2		; key 14 (@061) - PMT, f RND, g CFj
            then go to L12700
          p <- 10
          go to L12577
@@ -3976,7 +4000,7 @@ L12673:  if p = 11
 
 L12677:  a + 1 -> a[x]
 L12700:  a + 1 -> a[x]
-L12701:  a + 1 -> a[x]
+L12701:  a + 1 -> a[x]		; key 13 (@062) - PV, f NPV, g CF0
          a + 1 -> a[x]
 L12703:  if p = 11
            then go to L12524
@@ -4012,7 +4036,7 @@ L12737:  p <- 1
          load constant 13
          go to L12614
 
-L12742:  if p = 9
+L12742:  if p = 9		; key 73 (@101) - ., g std dev
            then go to L12760
          if p = 4
            then go to L12203
@@ -4021,7 +4045,7 @@ L12742:  if p = 9
          p <- 5
          go to L12411
 
-L12752:  if p # 2
+L12752:  if p # 2		; key 74 (@100) - R/S, f Sigma+, g Sigma-
            then go to L12600
          if 0 = s 0
            then go to L12010
@@ -4043,7 +4067,7 @@ L12764:  load constant 8
          delayed rom @04
          go to L12227
 
-L12775:  register -> c 14
+L12775:  register -> c 14	; key 34 (@160) - CLx, f CLR ALL, g CLP
          if p = 8
            then go to L13030
          if p # 10

ncd/spice/38c.ncd.tmpl
@@ -80,42 +80,43 @@ SPDX-License-Identifier: GPL-3.0-only
       </switch_pos>
     </switch>
 
-    <key user_keycode="11" hw_keycode="0064"/>
-    <key user_keycode="12" hw_keycode="0063"/>
-    <key user_keycode="13" hw_keycode="0062"/>
-    <key user_keycode="14" hw_keycode="0061"/>
-    <key user_keycode="15" hw_keycode="0060"/>
-
-    <key user_keycode="21" hw_keycode="0224"/>
-    <key user_keycode="22" hw_keycode="0223"/>
-    <key user_keycode="23" hw_keycode="0222"/>
-    <key user_keycode="24" hw_keycode="0221"/>
-    <key user_keycode="25" hw_keycode="0220"/>
-
-    <key user_keycode="31" hw_keycode="0163"/>
-    <key user_keycode="32" hw_keycode="0162"/>
-    <key user_keycode="33" hw_keycode="0161"/>
-    <key user_keycode="34" hw_keycode="0160"/>
-
-    <key user_keycode="41" hw_keycode="0243"/>
-    <key user_keycode="42" hw_keycode="0242"/>
-    <key user_keycode="43" hw_keycode="0241"/>
-    <key user_keycode="44" hw_keycode="0240"/>
-
-    <key user_keycode="51" hw_keycode="0143"/>
-    <key user_keycode="52" hw_keycode="0142"/>
-    <key user_keycode="53" hw_keycode="0141"/>
-    <key user_keycode="54" hw_keycode="0140"/>
-
-    <key user_keycode="61" hw_keycode="0323"/>
-    <key user_keycode="62" hw_keycode="0322"/>
-    <key user_keycode="63" hw_keycode="0321"/>
-    <key user_keycode="64" hw_keycode="0320"/>
-
-    <key user_keycode="71" hw_keycode="0103"/>
-    <key user_keycode="72" hw_keycode="0102"/>
-    <key user_keycode="73" hw_keycode="0101"/>
-    <key user_keycode="74" hw_keycode="0100"/>
+                                               <!-- unshifted  f-shifted  g-shifted -->
+    <key user_keycode="11" hw_keycode="0064"/> <!-- n          AMORT      12x       -->
+    <key user_keycode="12" hw_keycode="0063"/> <!-- i          INT        12/       -->
+    <key user_keycode="13" hw_keycode="0062"/> <!-- PV         NPV        CF0       -->
+    <key user_keycode="14" hw_keycode="0061"/> <!-- PMT        RND        CFj       -->
+    <key user_keycode="15" hw_keycode="0060"/> <!-- FV         IRR        Nj        -->
+					                                    	       
+    <key user_keycode="21" hw_keycode="0224"/> <!-- STO                   y^x       -->
+    <key user_keycode="22" hw_keycode="0223"/> <!-- RCL        %T         e^x       -->
+    <key user_keycode="23" hw_keycode="0222"/> <!-- %          Delta%     LN        -->
+    <key user_keycode="24" hw_keycode="0221"/> <!-- f                               -->
+    <key user_keycode="25" hw_keycode="0220"/> <!-- g                               -->
+					                                    	       
+    <key user_keycode="31" hw_keycode="0163"/> <!-- ENTER^     CLR PREFIX LASTx     -->
+    <key user_keycode="32" hw_keycode="0162"/> <!-- CHS        CLR FIN    EEX       -->
+    <key user_keycode="33" hw_keycode="0161"/> <!-- x<>y       CLR Sigma  Rdn       -->
+    <key user_keycode="34" hw_keycode="0160"/> <!-- CLx        CLR ALL    CLP       -->
+					                                    	       
+    <key user_keycode="41" hw_keycode="0243"/> <!-- -          Delta DAYS P/R       -->
+    <key user_keycode="42" hw_keycode="0242"/> <!-- 7                     GTO       -->
+    <key user_keycode="43" hw_keycode="0241"/> <!-- 8                     BST       -->
+    <key user_keycode="44" hw_keycode="0240"/> <!-- 9                     SST       -->
+					                                    	       
+    <key user_keycode="51" hw_keycode="0143"/> <!-- +          DATE       MEM       -->
+    <key user_keycode="52" hw_keycode="0142"/> <!-- 4                     PSE       -->
+    <key user_keycode="53" hw_keycode="0141"/> <!-- 5                     x<y       -->
+    <key user_keycode="54" hw_keycode="0140"/> <!-- 6                     x=0       -->
+					                                    	       
+    <key user_keycode="61" hw_keycode="0323"/> <!-- *          INTGR      FRAC      -->
+    <key user_keycode="62" hw_keycode="0322"/> <!-- 1                     lin est x -->
+    <key user_keycode="63" hw_keycode="0321"/> <!-- 2                     lin est y -->
+    <key user_keycode="64" hw_keycode="0320"/> <!-- 3                     n!        -->
+					                                    	       
+    <key user_keycode="71" hw_keycode="0103"/> <!-- /          1/x        weighted  -->
+    <key user_keycode="72" hw_keycode="0102"/> <!-- 0                     mean      -->
+    <key user_keycode="73" hw_keycode="0101"/> <!-- .                     std dev   -->
+    <key user_keycode="74" hw_keycode="0100"/> <!-- R/S        Sigma+     Sigma-    -->
   </keyboard>
 
   <obj_file addr_space="inst">

commit 5315194f85017f455d3000f48395450a5a1a9df2

Fix error introduced in 37E microcode in commit 3ae3d63.

ncd/spice/37e.asm
@@ -582,7 +582,7 @@ xft110:  a -> b[w]
            then go to xft120
          a + 1 -> a[x]
          if a >= c[x]
-           then go to xfgt130
+           then go to xft130
          c + 1 -> c[xs]
          if n/c go to L03066
 xft130:  0 -> c[w]