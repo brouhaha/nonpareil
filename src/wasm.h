36ca46cc4d5e5132f2252f80682659341006b736
Tim Allen <thristian@gmail.com>
2025-03-03 22:14:27 +1100

    Add missing src/wasm.h

---
+++ src/wasm.h
@@ -0,0 +1,28 @@
+/*
+Copyright 2023 Eric Smith <spacewar@gmail.com>
+SPDX-License-Identifier: GPL-3.0-only
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as
+published by the Free Software Foundation.
+
+Note that permission is NOT granted to redistribute and/or modify
+this porogram under the terms of any other version, earlier or
+later, of the GNU General Public License.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License version 3 for more details.
+
+You should have received a copy of the GNU General Public License
+version 3 along with this program (in the file "gpl-3.0.txt"); if not,
+see <https://www.gnu.org/licenses/>.
+*/
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "symtab.h"
+#include "asm.h"
+
+void pseudo_check(addr_t addr);
